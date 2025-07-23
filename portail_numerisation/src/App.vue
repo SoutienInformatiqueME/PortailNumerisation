8c7bdfa8429d6ffe400942c7ec494107969f7fdf
SoutienInformatiqueME <soutieninformatique@moissonestrie.com>
2025-07-23 13:54:15 -0400

    UI+

--- portail_numerisation/src/App.vue
+++ portail_numerisation/src/App.vue
@@ -3,10 +3,10 @@ import Navigation from './layouts/Navigation.vue';
 </script>
 
 <template>
-  <v-app>
-    <Navigation/>
+  <v-app style="background: linear-gradient(135deg, rgba(0, 255, 34, 0) 60%, rgba(245, 163, 39, 0.911));">
+    <Navigation />
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
--- portail_numerisation/src/layouts/Navigation.vue
+++ portail_numerisation/src/layouts/Navigation.vue
@@ -5,21 +5,20 @@ import { useRoute } from 'vue-router';
 <template>
     <!-- TODO : navigation mobile -->
     <!-- expand-on-hover :rail="$route.path !== '/PortailNumerisation/'" -->
-    <v-navigation-drawer permanent>
+    <v-navigation-drawer  expand-on-hover rail>
         <div>
             <v-list>
-                <v-list-item
+                <v-list-item :to="{ path: '/PortailNumerisation/' }"
                     prepend-avatar="https://www.moissonestrie.com/cdn/shop/files/3769_geodir_logo_12510294-597224803768615-8795392792948671030-n_32x32.png?v=1715785600"
                     subtitle="par Moisson Estrie" title="Portail Numérisation">
                 </v-list-item>
             </v-list>
         </div>
         <v-divider />
-        <v-list density="compact" nav>
-            <v-list-item prepend-icon="mdi-home" title="Accueil" :to="{ path: '/PortailNumerisation/' }"></v-list-item>
+        <v-list nav>
             <v-list-item prepend-icon="mdi-face-agent" title="Soutien"
                 :to="{ path: '/PortailNumerisation/Soutien' }"></v-list-item>
-            <v-list-item prepend-icon="mdi-school" title="Tutoriels"
+            <v-list-item ripple="orange" active-class="orange" prepend-icon="mdi-school" title="Tutoriels"
                 :to="{ path: '/PortailNumerisation/Tutoriels' }"></v-list-item>
             <v-list-item prepend-icon="mdi-ticket" title="Activités de formation"
                 :to="{ path: '/PortailNumerisation/ActivitesFormation' }"></v-list-item>
@@ -28,3 +27,11 @@ import { useRoute } from 'vue-router';
         </v-list>
     </v-navigation-drawer>
 </template>
+
+<style scoped>
+.v-list-item:hover,
+.v-list-item--active {
+    background-color: orange;
+    /* orange color */
+}
+</style>
