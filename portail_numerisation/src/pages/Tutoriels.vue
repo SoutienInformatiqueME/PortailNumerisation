ae2ebeba6949d554c516cc3c5939134fa496e9b5
SoutienInformatiqueME <soutieninformatique@moissonestrie.com>
2025-07-28 15:30:08 -0400

    Revamp

--- portail_numerisation/src/pages/Tutoriels.vue
+++ portail_numerisation/src/pages/Tutoriels.vue
@@ -1,21 +1,23 @@
 <script setup>
 import homepage from '@/assets/captures/connexion/homepage.png';
+import personne from '@/assets/captures/connexion/personne.png';
+import contacts from '@/assets/captures/connexion/contacts.png';
+import ajoutPersonne from '@/assets/captures/connexion/ajoutPersonne.png';
 </script>
 
 <template>
   <v-card>
     <v-tabs v-model="tab">
       <v-tab value="Connexion">Connexion</v-tab>
-      <v-tab value="two">Item Two</v-tab>
-      <v-tab value="three">Item Three</v-tab>
     </v-tabs>
 
     <v-card-text>
       <v-tabs-window v-model="tab">
         <v-tabs-window-item value="Connexion">
           <v-card>
-            <v-card-title>Rejoindre GoToucan en tant que première personne de votre organisme à vous y
-              connecter</v-card-title>
+            <v-card-title>
+              Rejoindre GoToucan et la communauté de votre organisme à vous y connecter
+            </v-card-title>
             <v-card-text>
               <span>
                 Vous devez d'abord communiquer avec Moisson Estrie (<a target="_blank"
@@ -31,20 +33,18 @@ import homepage from '@/assets/captures/connexion/homepage.png';
               </span>
             </v-card-text>
           </v-card>
-          <v-card>
+          <!-- <v-card>
             <v-card-title>Inviter d'autres personnes à rejoindre votre organisme sur GoToucan</v-card-title>
-            <v-card-text>
-              <div>
-                <span>Connectez-vous à votre compte sur GoToucan puis sélectionnez la communauté de votre organisme.</span>
-                <v-img contain :src="homepage" alt="homepage.png"></v-img>
-              </div> 
-              <br /><span>Pour inviter quelqu'un, vous devez d'abord faire la création de cette personne en utilisant le
-                module Contact (sauf si vous l'avez précédement fait).</span>
-              <br /><span>Une fois la personne crée, assurez-vous d'être sur la page de la personne puis sélectionnez la "baguette magique".</span>
-              <br /><span>Sélectionnez l'outil "Invitations".</span>
-              <br /><span>Suivez les instructions à l'écran pour finaliser l'invitation.</span>
-            </v-card-text>
-          </v-card>
+            <v-card-text class="d-flex flex-wrap">
+              <template v-for="(step, index) in steps[0].invitation" :key="index">
+                <v-card class="step">
+                  <span>{{ index + 1 }}. </span>
+                  <span>{{ step.text }}</span>
+                  <v-img :src="step.image" />
+                </v-card>
+              </template>
+</v-card-text>
+</v-card> -->
         </v-tabs-window-item>
 
         <v-tabs-window-item value="two">
@@ -58,10 +58,58 @@ import homepage from '@/assets/captures/connexion/homepage.png';
     </v-card-text>
   </v-card>
 </template>
+
 <script>
 export default {
   data: () => ({
     tab: null,
+    // steps: [
+    //   {
+    //     invitation: [
+    //       {
+    //         text: 'Ensuite, vous devez faire la création de cette personne en utilisant le module Contact (sauf si la personne existe déjà dans vos contacts).',
+    //         image: contacts,
+    //       },
+    //       {
+    //         text: 'Une fois la personne créée, assurez-vous d\'être sur la page de la personne puis sélectionnez la "baguette magique".',
+    //         image: personne,
+    //       },
+    //       {
+    //         text: 'Sélectionnez l\'outil "Invitations".',
+    //         image: ajoutPersonne,
+    //       },
+    //       {
+    //         text: 'Suivez les instructions à l\'écran pour finaliser l\'invitation',
+    //         image: ajoutPersonne,
+    //       },
+    //     ],
+    //     first: [
+    //       {
+    //         text: 'Ensuite, vous devez faire la création de cette personne en utilisant le module Contact (sauf si la personne existe déjà dans vos contacts).',
+    //         image: contacts,
+    //       },
+    //       {
+    //         text: 'Une fois la personne créée, assurez-vous d\'être sur la page de la personne puis sélectionnez la "baguette magique".',
+    //         image: personne,
+    //       },
+    //       {
+    //         text: 'Sélectionnez l\'outil "Invitations".',
+    //         image: ajoutPersonne,
+    //       },
+    //       {
+    //         text: 'Suivez les instructions à l\'écran pour finaliser l\'invitation',
+    //         image: ajoutPersonne,
+    //       },
+    //     ],
+    //   },
+    // ],
   }),
 }
 </script>
+
+<style scoped>
+.step {
+  max-height: 50vh;
+  max-width: 25vw;
+}
+</style>
