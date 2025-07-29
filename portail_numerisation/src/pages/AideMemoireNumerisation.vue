49fd3dcb6cc2b3ad37a112437cd451a3faf645e7
SoutienInformatiqueME <soutieninformatique@moissonestrie.com>
2025-07-29 09:01:20 -0400

    Update AideMemoireNumerisation.vue

--- portail_numerisation/src/pages/AideMemoireNumerisation.vue
+++ portail_numerisation/src/pages/AideMemoireNumerisation.vue
@@ -2,8 +2,8 @@
   <v-container fluid class="fill-height pa-0">
     <v-card class="d-flex flex-column fill-height w-100">
       <v-card-title>Aide-Mémoire Numérisation</v-card-title>
-      <v-card-text class="flex-grow-1 pa-0" style="height: 80vh; width: 95%">
-      <iframe :src="'/docs/aide-mémoire-numérisation.pdf'" type="application/pdf" width="100%" height="100%" style="border: none"></iframe>  
+      <v-card-text class="flex-grow-1 pa-0" style="height: 80vh; width: 160vh">
+        <iframe :src="'./docs/aide-mémoire-numérisation.pdf'" type="application/pdf" width="100%" height="100%" />
       </v-card-text>
     </v-card>
   </v-container>
