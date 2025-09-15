de3931640ff8e3bcd343d5a50ddec4cb97473c76
SoutienInformatiqueME <soutieninformatique@moissonestrie.com>
2025-09-15 17:10:50 -0400

    history

--- portail/quasar.config.js
+++ portail/quasar.config.js
@@ -40,14 +40,14 @@ export default defineConfig((/* ctx */) => {
         node: 'node20'
       },
 
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/',
+      publicPath: '/PortailNumerisation/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
