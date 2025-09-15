153a578b4954020388ad9b25a37249298c4a41cd
SoutienInformatiqueME <soutieninformatique@moissonestrie.com>
2025-09-15 16:08:56 -0400

    routes

--- portail/src/router/routes.js
+++ portail/src/router/routes.js
@@ -1,26 +1,39 @@
 const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-  {
-    path: '/informations',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/InformationsGenerales.vue') }],
-  },
-  {
-    path: '/soutien',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/SoutienNumerisation.vue') }],
-  },
+    {
+        path: '/',
+        component: () => import ('layouts/MainLayout.vue'),
+        children: [
+            {
+                path: '',
+                component: () => import ('pages/IndexPage.vue')
+            }
+        ]
+    }, {
+        path: '/informations',
+        component: () => import ('layouts/MainLayout.vue'),
+        children: [
+            {
+                path: '',
+                component: () => import ('pages/InformationsGenerales.vue')
+            }
+        ]
+    }, {
+        path: '/soutien',
+        component: () => import ('layouts/MainLayout.vue'),
+        children: [
+            {
+                path: '',
+                component: () => import ('pages/SoutienNumerisation.vue')
+            }
+        ]
+    },
 
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+        path: '/:catchAll(.*)*',
+        component: () => import ('pages/ErrorNotFound.vue')
+    },
 ]
 
 export default routes
