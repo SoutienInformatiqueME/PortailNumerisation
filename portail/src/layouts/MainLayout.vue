c972f64b59f55cabe372b05f0aa7e7ae5f08e8f8
SoutienInformatiqueME <soutieninformatique@moissonestrie.com>
2025-09-15 16:27:04 -0400

    test MainLayout (styles)

--- portail/src/layouts/MainLayout.vue
+++ portail/src/layouts/MainLayout.vue
@@ -5,25 +5,3 @@
     </q-page-container>
   </q-layout>
 </template>
-
-<style scoped>
-@keyframes breathe {
-  0%,
-  100% {
-    background-color: var(--q-primary);
-    transform: scale(1);
-  }
-  50% {
-    background-color: var(--q-secondary);
-    transform: scale(1.05);
-  }
-}
-
-.breathing {
-  animation: breathe 10s ease-in-out infinite;
-}
-
-.active {
-  color: var(--q-secondary);
-}
-</style>
